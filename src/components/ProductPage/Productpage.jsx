1fb35082f4c423af14452105ec83e0906bd1d76d
artisonii <artisoni2615@gmail.com>
2023-12-02 22:20:43 +0530

    filter part and discount

--- src/components/ProductPage/Productpage.jsx
+++ src/components/ProductPage/Productpage.jsx
@@ -1,58 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import style from "./ProductPage.module.css"
+import style from "./ProductPage.module.css";
 
-import percentage from "./images/percentageIcon.svg"
-import flash from "./images/flash.svg"
+import percentage from "./images/percentageIcon.svg";
+import flash from "./images/flash.svg";
 
-
-
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
-const baseUrl = "https://jiomart-6f8ef-default-rtdb.firebaseio.com/products.json"
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+const baseUrl =
+    "https://jiomart-6f8ef-default-rtdb.firebaseio.com/products.json";
 
 function valuetext(value) {
-    return `${value}°C`;
+    return `${value}`;
 }
 
 const Productpage = () => {
-
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
     const [brand, setBrand] = useState("");
 
     const getData = async (url) => {
         try {
             const res = await axios.get(url);
-            console.log(res.data)
-            setData(res.data)
+            console.log(res.data);
+            setData(res.data);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-    }
+    };
     useEffect(() => {
-        getData(baseUrl)
+        getData(baseUrl);
+    }, []);
 
-    }, [])
-
-    console.log(brand)
+    console.log(brand);
 
     const [value, setValue] = React.useState([5000, 70000]);
+    const [valueDis, setValueDis] = React.useState([3, 50]);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
+    const handleChangeDis = (event, newValueDis) => {
+        setValueDis(newValueDis);
+    };
 
-
-    console.log(value)
+    console.log(value);
+    console.log(valueDis);
 
     return (
         <div className={style.product_page_container}>
             <div className={style.address}>
                 <h6>
                     <span>Home</span> {" > "}
-                    <span>All Categories</span>{" > "}
-                    <span>Electronics</span>{" > "}
+                    <span>All Categories</span>
+                    {" > "}
+                    <span>Electronics</span>
+                    {" > "}
                     <span> Mobiles & Tablets</span>
                 </h6>
             </div>
@@ -61,30 +64,73 @@ const Productpage = () => {
                     <div>
                         <h2>Filters</h2>
 
-                        <div><h4>brands</h4>
+                        <div>
+                            <h4>brands</h4>
 
                             <label htmlFor="Apple">
-                                <input type="radio" name="brand" value="Apple" onChange={(e) => { setBrand(e.target.value) }} /> Apple
+                                <input
+                                    type="checkbox"
+                                    name="brand"
+                                    value="Apple"
+                                    onChange={(e) => {
+                                        setBrand(e.target.value);
+                                    }}
+                                />{" "}
+                                Apple
                             </label>
                             <br />
 
                             <label htmlFor="Poco">
-                                <input type="radio" name="brand" value="Poco" onChange={(e) => { setBrand(e.target.value) }} /> Poco
+                                <input
+                                    type="checkbox"
+                                    name="brand"
+                                    value="Poco"
+                                    onChange={(e) => {
+                                        setBrand(e.target.value);
+                                    }}
+                                />{" "}
+                                Poco
                             </label>
                             <br />
 
                             <label htmlFor="Tecno">
-                                <input type="radio" name="brand" value="Tecno" onChange={(e) => { setBrand(e.target.value) }} /> Tecno
-                            </label><br />
+                                <input
+                                    type="checkbox"
+                                    name="brand"
+                                    value="Tecno"
+                                    onChange={(e) => {
+                                        setBrand(e.target.value);
+                                    }}
+                                />{" "}
+                                Tecno
+                            </label>
+                            <br />
 
                             <label htmlFor="OnePlus">
-                                <input type="radio" name="brand" value="OnePlus" onChange={(e) => { setBrand(e.target.value) }} /> One Plus
-                            </label><br />
+                                <input
+                                    type="checkbox"
+                                    name="brand"
+                                    value="OnePlus"
+                                    onChange={(e) => {
+                                        setBrand(e.target.value);
+                                    }}
+                                />{" "}
+                                One Plus
+                            </label>
+                            <br />
 
-                            <label htmlFor='Xiomi'>
-                                <input type="radio" name="brand" value="Xiomi" onChange={(e) => { setBrand(e.target.value) }} /> Xiomi
-
-                            </label><br />
+                            <label htmlFor="Xiomi">
+                                <input
+                                    type="checkbox"
+                                    name="brand"
+                                    value="Xiomi"
+                                    onChange={(e) => {
+                                        setBrand(e.target.value);
+                                    }}
+                                />{" "}
+                                Xiaomi
+                            </label>
+                            <br />
                         </div>
 
                         <hr />
@@ -92,12 +138,11 @@ const Productpage = () => {
                         <div className={style.slider}>
                             <h5>Price Range</h5>
                             <Slider
-                                getAriaLabel={() => 'Temperature range'}
+                                getAriaLabel={() => "Price range"}
                                 value={value}
                                 onChange={handleChange}
                                 valueLabelDisplay="auto"
                                 getAriaValueText={valuetext}
-
                                 min={1000} // set the minimum value
                                 max={100000} // set the maximum value
                             />
@@ -108,65 +153,62 @@ const Productpage = () => {
                         <div className={style.dis}>
                             <h5>Discount Range</h5>
                             <Slider
-                                getAriaLabel={() => 'Temperature range'}
-                                value={value}
-                                onChange={handleChange}
+                                getAriaLabel={() => "Discount range"}
+                                value={valueDis}
+                                onChange={handleChangeDis}
                                 valueLabelDisplay="auto"
                                 getAriaValueText={valuetext}
-
                                 min={0} // set the minimum value
                                 max={60} // set the maximum value
                             />
                         </div>
 
                         <hr />
-
-
-
-
-
                     </div>
-
-
-
                 </div>
-
-
 
                 <div className={style.right_container}>
                     <div>
                         <select>
-                            <option value="">Sort by: <b>Popularity</b></option>
+                            <option value="">
+                                Sort by: <b>Popularity</b>
+                            </option>
                             <option value="asc">Price low to high</option>
                             <option value="desc">Price high to low</option>
                         </select>
-
                     </div>
                     <div className={style.product}>
-                        {
-                            data.map((ele) => {
-                                return (
-                                    <div>
-                                        <div className={style.imagediv}><img src={ele.image} alt="image" className={style.pic} /></div>
-                                        <div>
-
-                                            <p>{ele.title}</p>
-                                            <p><b>{ele.price}</b> <span className={style.line_through}>₹{Math.ceil((ele.price * 100) / (100 - ele.discount))}</span> <span>{ele.discount}% OFF</span></p>
-                                            <p>
-                                                <img src={percentage} alt="percentage" /><span>Exchange Offer Available</span><br />
-                                                <img src={flash} alt="flash" /><span>Limited Time Deal</span>
-
-                                            </p>
-                                        </div>
+                        {data.map((ele) => {
+                            return (
+                                <div>
+                                    <div className={style.imagediv}>
+                                        <img src={ele.image} alt="image" className={style.pic} />
                                     </div>
-                                )
-                            })
-                        }
+                                    <div>
+                                        <p>{ele.title}</p>
+                                        <p>
+                                            <b>{ele.price}</b>{" "}
+                                            <span className={style.line_through}>
+                                                ₹{Math.ceil((ele.price * 100) / (100 - ele.discount))}
+                                            </span>{" "}
+                                            <span>{ele.discount}% OFF</span>
+                                        </p>
+                                        <p>
+                                            <img src={percentage} alt="percentage" />
+                                            <span>Exchange Offer Available</span>
+                                            <br />
+                                            <img src={flash} alt="flash" />
+                                            <span>Limited Time Deal</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Productpage
+export default Productpage;
