1a41530ace61141f60b0a81254f1502792a33757
Mrinal26 <Mrinalkumar5046@gmail.com>
2023-12-03 15:23:48 +0530

    uploade

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Homepage from './pages/Homepage'
-import Navbar from './components/Navbar/Navbar'
+import {Navbar} from './components/Navbar/Navbar'
 import AllRoutes from './allRoutes/AllRoutes'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   
 
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
      <AllRoutes/>
-   
+    <Footer/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
+// import store from "./redux/store.js"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
     <BrowserRouter>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
