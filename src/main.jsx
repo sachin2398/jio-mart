4754fd9d9ffe44c9fa8337ccf70fa152b112f43c
satyam46020 <satyam46020@gmail.com>
2023-12-03 13:17:26 +0530

    cart

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+  
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,11 @@
-import {legacy_createStore, applyMiddleware , combineReducers} from "redux"
-import thunk from "redux-thunk"
+import { combineReducers, legacy_createStore } from "redux";
+import AuthReducer from "../components/Login/redux/AuthReducer";
+
+// import ProductReducer from "../components/ProductPage/ProductReducer/productReducer";
+// import ProductDetailReducer from "../components/ProductPage/Redux/ProductDetailReducer/ProductDetailReducer";
 
 
-const store = legacy_createStore()
-
-export default store
-
-
-//cart => ADD, REMOVE, INC QTY, DEC QTY
-//with localstorage
-
-//Auth => LOGIN, SIGNUP
-//with localstorage
-
-
-
-
+import CartReducer from "../components/CartPage/redux/CartReducers";
+const combinereducers = combineReducers({ AuthReducer, CartReducer });
+ const store = legacy_createStore(combinereducers);
+ export default store;
