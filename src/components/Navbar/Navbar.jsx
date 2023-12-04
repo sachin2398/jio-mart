93dbb720176ac0cbf2a8d9bc78f2b613cf1ffb25
artisonii <artisoni2615@gmail.com>
2023-12-04 14:10:34 +0530

    fixed productpage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,28 +7,6 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className={styles.nav}>
-<<<<<<< HEAD
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-=======
       Navbar
       <h1> Navbar</h1>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -61,19 +39,14 @@ const Navbar = () => {
                   aria-current="page"
                   href="#"
                 >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                   Home
                 </a>
               </li>
               <li className="nav-item">
-<<<<<<< HEAD
-                <a className="nav-link" href="#">
-=======
                 <a
                   className="nav-link"
                   href="#"
                 >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                   Link
                 </a>
               </li>
@@ -89,26 +62,18 @@ const Navbar = () => {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-<<<<<<< HEAD
-                    <a className="dropdown-item" href="#">
-=======
                     <a
                       className="dropdown-item"
                       href="#"
                     >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                       Action
                     </a>
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <a className="dropdown-item" href="#">
-=======
                     <a
                       className="dropdown-item"
                       href="#"
                     >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                       Another action
                     </a>
                   </li>
@@ -116,73 +81,47 @@ const Navbar = () => {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <a className="dropdown-item" href="#">
-=======
                     <a
                       className="dropdown-item"
                       href="#"
                     >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                       Something else here
                     </a>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-<<<<<<< HEAD
-                <a className="nav-link disabled" aria-disabled="true">
-=======
                 <a
                   className="nav-link disabled"
                   aria-disabled="true"
                 >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                   Disabled
                 </a>
               </li>
             </ul>
-<<<<<<< HEAD
-            <form className="d-flex" role="search">
-=======
             <form
               className="d-flex"
               role="search"
             >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-<<<<<<< HEAD
-              <button className="btn btn-outline-success" type="submit">
-=======
               <button
                 className="btn btn-outline-success"
                 type="submit"
               >
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
                 Search
               </button>
             </form>
           </div>
         </div>
       </nav>
-<<<<<<< HEAD
-
-
-    </div>
-  )
-}
-
-export default Navbar
-=======
     </div>
   );
 };
->>>>>>> 60774ac13346e9dc128468b59e0fe4c2637cbf3b
 
 export default Navbar;
 
